a4488ba09b629a782467050e905ecd20480eac59
Gustavo Santana <98903011+gugasant@users.noreply.github.com>
2022-09-12 22:23:30 -0300

    Add files via upload

---
+++ javascript/db.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('testesistema', 'root', '123456', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function(){
+    console.log('Conectado com sucesso!')
+}).catch(function(erro){
+    console.log('Falha ao se conectar: '+erro)
+})
+
+module.exports = sequelize
