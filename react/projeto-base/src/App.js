9e6ef737df00f4f193e2aadc4bcaad89252d1384
gugasant <gustavosantlopes3@gmail.com>
2022-09-30 19:17:22 -0300

    teste2

---
+++ react/projeto-base/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
+        <p>Meu primeiro App React</p>
+        <p>Made by Gustavo Santana</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
