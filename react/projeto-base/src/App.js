d21326128d037479ae35d1df0832aa0bae61ccf6
gugasant <gustavosantlopes3@gmail.com>
2022-10-01 00:55:59 -0300

    teste3

--- react/projeto-base/src/App.js
+++ react/projeto-base/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
-        <p>Meu primeiro App React</p>
         <p>Made by Gustavo Santana</p>
         <HelloWorld />
+        <SayMyName nome="Visitante" />
+        <Pessoa 
+        nome="Gustavo" 
+        idade="30" 
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150"
+        />
       </header>
     </div>
   );
