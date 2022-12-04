d64647479b320e2500afdea024769131c1ca6a07
gugasant <gustavosantlopes3@gmail.com>
2022-12-04 01:05:37 -0300

    implementando componente com roteamento e mudando todo o app

--- react/projeto-base/src/App.js
+++ react/projeto-base/src/App.js
@@ -1,30 +1,9 @@
-import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import { useState } from 'react';
+import React from 'react';
+import Routes from "./routes";
 
-function App() {
 
-  const [number, setNumber] = useState(1);
-
-  const changeNumber = () => {
-    setNumber((prevNumber) => prevNumber + 1);
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Made by Gustavo Santana</p>
-        <HelloWorld />
-        <SayMyName nome="Visitante" />
-        <div>
-        <p>Número: {number}</p>
-        <button onClick={changeNumber}>Aumentar número</button>
-      </div>
-      </header>
-    </div>
+    <Routes/>
   );
 }
-
-export default App;
