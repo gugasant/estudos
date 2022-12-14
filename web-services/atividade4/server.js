6c7b98dba7e9ec59d96d28c90ba95e63706d9c87
gugasant <gustavosantlopes3@gmail.com>
2022-12-13 23:14:27 -0300

    atividade4 ws

---
+++ web-services/atividade4/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+// GET - READ
+app.get("/livros", function(req, res) {
+    res.json(data);
+})
+
+app.get("/livros/:id", function(req, res) {
+    const { id } = req.params;
+    const livro = data.find(liv => liv.id == id);
+
+    if(!client) return res.status(204).json();
+
+    res.json(livro);
+});
+
+// POST - CREATE
+app.post("/livros", function(req, res) {
+    const { nome, autor, isbn } = req.body;
+    // salvar
+    res.json({ nome, autor, isbn });
+});
+
+// PUT - UPDATE
+app.put("/livros/:id", function(req, res) {
+    const { id } = req.params;
+    const livro = data.find(liv => liv.id == id);
+
+    if (!livro) return res.status(204).json();
+
+    const { nome } = req.body;
+
+    livro.nome = nome
+    res.json(livro);
+});
+
+// DELETE 
+app.delete("/livros/:id", function(req, res) {
+    const { id } = req.params;
+    const livrosFiltered = data.filter(livro => livro.id != id);
+
+    res.json(livrosFiltered);
+});
+
+app.listen(3000, function() {
+    console.log("Server is running!");
+});
