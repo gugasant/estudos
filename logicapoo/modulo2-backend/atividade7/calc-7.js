74c1d29d6ca5008c15b19a6361393e21fd0af379
gugasant <gustavosantlopes3@gmail.com>
2023-07-31 17:10:12 -0300

    a7-m2

--- logicapoo/modulo2-backend/atividade7/calc-7.js
+++ logicapoo/modulo2-backend/atividade7/calc-7.js
@@ -1,4 +1,3 @@
-// Fiz a atividade em JS pois o tutor Danilo disse que não teria problemas e seria até melhor nesse momento
 const prompt = require("prompt-sync")({sigint:false});
 
 function calcNum(operacao, numero1, numero2) {
@@ -10,9 +9,7 @@ if (operacao == 1) {
     return numero1 * numero2
   } else if (operacao == 4) {
     return numero1 / numero2
-  } else if (operacao > 4) {
-    console.log("INVALIDO")
-  }
+  } 
 }
 
 operacao = parseInt(prompt("1-SOMAR 2-SUBTRAIR 3-MULTIPLICAR 4-DIVIDIR 0-SAIR "))
