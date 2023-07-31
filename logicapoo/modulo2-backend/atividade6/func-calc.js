d19ef862576e709a34774f9d9c5b08a64da99a63
gugasant <gustavosantlopes3@gmail.com>
2023-07-31 00:31:34 -0300

    m2-a6

--- logicapoo/modulo2-backend/atividade6/func-calc.js
+++ logicapoo/modulo2-backend/atividade6/func-calc.js
@@ -1,16 +1,16 @@
 function calcNum(numero1, numero2, operacao) {
   if (operacao == 1) {
-      return resultado = numero1 + numero2
+    return numero1 + numero2
   } else if (operacao == 2) {
-      return resultado = numero1 - numero2
+    return numero1 - numero2
   } else if (operacao == 3) {
-      return resultado = numero1 * numero2
+    return numero1 * numero2
   } else if (operacao == 4) {
-      return resultado = numero1 / numero2
+    return numero1 / numero2
   } else {
       return 0
   }
 };
 
-console.log(calcNum(5, 5, 3))
-
+let resultado = calcNum(15, 3, 4);
+console.log(resultado);
