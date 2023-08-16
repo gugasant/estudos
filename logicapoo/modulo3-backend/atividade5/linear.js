3a70cbcbfe5f40c58175d9594d5afabbb3c7abcf
Gustavo Santana <98903011+gugasant@users.noreply.github.com>
2023-08-16 14:52:43 -0300

    a5-m3

---
+++ logicapoo/modulo3-backend/atividade5/linear.js
@@ -0,0 +1,19 @@
+function acharNumero(array, numeroBuscado) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === numeroBuscado) {
+            return i
+        } 
+    }
+    return -1
+}
+
+const lista = [15, 8, 10, 25, 12, 30, 5, 20, 18, 7];
+const numero = 20;
+
+const indice = acharNumero(lista, numero);
+
+if (indice !== -1) {
+    console.log(`O número ${numero} está no índice ${indice} da lista`)
+} else {
+    console.log(`O número ${numero} não está na lista`)
+}
