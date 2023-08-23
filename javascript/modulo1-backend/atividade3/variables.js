2b32335c8d27ef5d6777722ed188c340786d2101
gugasant <gustavosantlopes3@gmail.com>
2023-08-23 09:35:15 -0300

    js-m1-a3

---
+++ javascript/modulo1-backend/atividade3/variables.js
@@ -0,0 +1,29 @@
+let nome;
+let numerogrande;
+let logico;
+let simbolo;
+
+console.log(nome);
+console.log(numerogrande);
+console.log(logico);
+console.log(simbolo);
+
+nome = null;
+numerogrande = null;
+logico = null;
+simbolo = null;
+
+console.log(nome);
+console.log(numerogrande);
+console.log(logico);
+console.log(simbolo);
+
+nome = 'Maria';
+numerogrande = 987654321987n;
+logico = true;
+simbolo = Symbol();
+
+console.log(typeof nome);
+console.log(typeof numerogrande);
+console.log(typeof logico);
+console.log(typeof simbolo);
